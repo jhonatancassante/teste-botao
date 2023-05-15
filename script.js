b520634e9c67f79b2312a4b035d7e01e91c6087d
Jhonatan Casssante de Brito <jhonatan.cassante@live.com>
2023-05-15 16:11:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let notButton = document.getElementById("notButton");
 let yesButton = document.getElementById("yesButton");
 
 notButton.addEventListener("mousemove", (e) => {
-  let randomPositionTop = Math.floor(Math.random() * 900);
-  let randomPositionRight = Math.floor(Math.random() * 1500);
-  let randomPositionBottom = Math.floor(Math.random() * 900);
-  let randomPositionLeft = Math.floor(Math.random() * 1500);
+  let randomPositionTop = Math.floor(Math.random() * 300);
+  let randomPositionRight = Math.floor(Math.random() * 500);
+  let randomPositionBottom = Math.floor(Math.random() * 300);
+  let randomPositionLeft = Math.floor(Math.random() * 500);
 
   notButton.style.top = `${randomPositionTop}px`;
   notButton.style.left = `${randomPositionRight}px`;
